0dbf764a6af3f6d5acc711429e322a24c9669828
Andreas <git@fsck.rip>
2024-11-08 20:08:00 -0500

    basic getEvent logic
    Signed-off-by: Andreas <git@fsck.rip>

--- src/server/api/routers/events/getEvent.ts
+++ src/server/api/routers/events/getEvent.ts
@@ -1,13 +1,29 @@
 import axios from "axios";
 import { publicProcedure } from "../../trpc";
 import z from "zod";
+import { EventResult, EventSource, Event_Involved } from "~/types/Event";
+import { getInvolvedEvent, getInvolvedEvents } from "~/utils/involved-events";
+import { transformEvent } from "~/utils/transform-event";
+import { getTUEvent } from "~/utils/tu-events";
 
 export const getEvent = publicProcedure
-  .input(z.string())
+  .input(
+    z.object({
+      id: z.string(),
+      source: z.enum(["events", "involved"]).optional(),
+    }),
+  )
   .query(async ({ ctx, input }) => {
-    const { data } = await axios.get<{data: {start: Date}}>(
-      `https://events.towson.edu/api/2/events/${input}`,
-    );
+    let event: EventResult | null | undefined = null;
+    if (input.source === EventSource.INVOLVED) {
+      event = transformEvent(
+        (await getInvolvedEvent(input.id)),
+        EventSource.INVOLVED,
+      );
+    } else {
+      const original = await getTUEvent(input.id);
+      event = transformEvent(original, EventSource.INVOLVED);
+    }
 
-    return data;
+    return event;
   });
