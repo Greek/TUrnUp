abf969093c38ecbedf8bc1845f08d5bba97fdae7
Andreas <git@fsck.rip>
2024-11-09 13:57:21 -0500

    add OrgResult
    Signed-off-by: Andreas <git@fsck.rip>

--- src/types/Organization.ts
+++ src/types/Organization.ts
@@ -1,10 +1,18 @@
 export interface Org_Involved {
-  id: string | number,
-  name: string,
-  shortName: string,
-  nameSortKey: string,
-  websiteKey: string,
-  summary: string,
-  profilePicture: string,
+  id: string | number;
+  name: string;
+  shortName: string;
+  nameSortKey: string;
+  websiteKey: string;
+  summary: string;
+  profilePicture: string;
 }
-export interface Org_TUEvents {}
+
+export interface OrgResult {
+  name: any;
+  shortName: any;
+  nameSortKey: any;
+  originalUrl: string;
+  summary: any;
+  profilePicture: string | null;
+}
