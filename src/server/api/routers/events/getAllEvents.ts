b40da9f6b3856805af1e8dd70569caf995a968aa
Andreas <git@fsck.rip>
2025-08-03 13:10:05 -0700

    api(getAllEvents): graceful fetching
    Signed-off-by: Andreas <git@fsck.rip>

--- src/server/api/routers/events/getAllEvents.ts
+++ src/server/api/routers/events/getAllEvents.ts
@@ -2,18 +2,29 @@ import { publicProcedure } from "../../trpc";
 import z from "zod";
 import type { EventResult } from "~/types/Event";
 import { getRedisInstance } from "~/lib/redis";
+import { TRPCError } from "@trpc/server";
 
 export const getAllEvents = publicProcedure
   .input(z.object({ limit: z.number() }).optional())
   .query(async ({}) => {
     const currDate = new Date().toISOString().slice(0, 10).replace(/-/g, "");
-    console.log(currDate);
     const redis = getRedisInstance();
-    const dataString = (await redis.get(`snapshot:${currDate}:towson`))!;
-    let data: EventResult[];
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    data = JSON.parse(dataString);
+    let redisValue: string;
+    try {
+      redisValue = (await redis.get(`snapshot:${currDate}:towson`)) ?? "";
+    } catch (err: unknown) {
+      console.error(err);
+      throw new TRPCError({
+        message: "Failed to get all events",
+        code: "NOT_FOUND",
+      });
+    }
+
+    let data: EventResult[] = (
+      redisValue ? JSON.parse(redisValue) : []
+    ) as EventResult[];
+
     data = data
       .filter(
         (e) =>
