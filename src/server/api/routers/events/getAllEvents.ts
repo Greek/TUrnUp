58b157bc0723da8ee0bf66583bede3c01eb82d53
Andreas <git@fsck.rip>
2025-08-03 12:41:02 -0700

    api: use redis to get all events
    Signed-off-by: Andreas <git@fsck.rip>

--- src/server/api/routers/events/getAllEvents.ts
+++ src/server/api/routers/events/getAllEvents.ts
@@ -1,36 +1,20 @@
 import { publicProcedure } from "../../trpc";
 import z from "zod";
-import {
-  transformInvolvedEvent,
-  transformTUEvent,
-} from "~/utils/transform-event";
-import { getTUEvents } from "~/utils/tu-events";
-import type { Event_Involved } from "~/types/Event";
-import { getInvolvedEvent, getInvolvedEvents } from "~/utils/involved-events";
+import type { EventResult } from "~/types/Event";
+import { getRedisInstance } from "~/lib/redis";
 
 export const getAllEvents = publicProcedure
   .input(z.object({ limit: z.number() }).optional())
   .query(async ({}) => {
-    const initialEventsData = await getTUEvents();
-    const initialInvolvedData = await getInvolvedEvents();
+    const currDate = new Date().toISOString().slice(0, 10).replace(/-/g, "");
+    console.log(currDate);
+    const redis = getRedisInstance();
+    const dataString = (await redis.get(`snapshot:${currDate}:towson`))!;
+    let data: EventResult[];
 
-    const eventsData = await Promise.all(
-      initialEventsData.events.map(async (entry) =>
-        transformTUEvent(entry.event),
-      ),
-    );
-
-    const involvedData = await Promise.all(
-      initialInvolvedData.map(async (entry: Event_Involved) => {
-        // getting all involved events won't fetch some critical data,
-        // so we'll fetch all individual events and transform them
-        // with all the data from the query.
-        const event = await getInvolvedEvent(entry.id);
-        return transformInvolvedEvent(event);
-      }),
-    );
-
-    const finalData = [...involvedData, ...eventsData]
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    data = JSON.parse(dataString);
+    data = data
       .filter(
         (e) =>
           e.start_date &&
@@ -44,5 +28,5 @@ export const getAllEvents = publicProcedure
         else return -1;
       });
 
-    return finalData;
+    return data;
   });
